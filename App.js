09877023b6d4a01d7147aae4dacdbc46839d7e27
Alfred <alfred.ahonen@gmail.com>
2023-03-26 00:12:13 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+  const [hasContactsPermission, setContactsPermission] = useState(null);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    askContactsPermission();
+  }, []);
+
+  const askContactsPermission = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    setContactsPermission( status == 'granted' );
+  }
+
+  const getContacts = async () => {
+    const { data } = await Contacts.getContactsAsync();
+
+    if (data.length > 0) {
+      setContacts(data);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={contacts}
+        renderItem={({item}) => {
+          if (item.phoneNumbers === undefined) {
+            return (<Text>{item.name}</Text>);
+          } else {
+            return (<Text>{item.name} {item.phoneNumbers[0].number}</Text>);
+          }
+        }}
+        keyExtractor={item => item.id}
+      />
+      {hasContactsPermission ? (
+        <Button title="Get contacts" onPress={getContacts} />
+      ) : (
+        <Text>No access to contacts</Text>
+      )}
     </View>
   );
 }
